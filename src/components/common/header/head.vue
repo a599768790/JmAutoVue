d6dd9756d83414da3397be05b9b6c82b5f8f8c0a
599768790@qq.com <599768790@qq.com>
2019-04-22 23:39:39 +0800

    04222339

--- src/components/common/header/head.vue
+++ src/components/common/header/head.vue
@@ -29,9 +29,9 @@
 <style lang="scss" scoped>
   .header-top{
     text-align: center;
-    height: 0.5rem;
+    height: 0.25rem;
     background: #26a2ff;
-    line-height: 0.5rem;
+    line-height: 0.25rem;
     .text{
       font-size:0.18rem;
       color:#fff;
--- src/components/common/swipe/Swipe.vue
+++ src/components/common/swipe/Swipe.vue
@@ -1,7 +1,8 @@
 <template>
  <div>
-   111
-   <div>222</div>
+   <div class="swipe">
+     <img src="@/assets/pic/xiq_02.png">
+   </div>
     <!-- <mt-swipe :auto="0">
         <mt-swipe-item class="one">1</mt-swipe-item>
         <mt-swipe-item class="two">2</mt-swipe-item>
@@ -23,21 +24,35 @@
  }
 </script>
 
-<style lang="stylus">
+<style lang="scss" scoped>
+.swipe{
+  width:100;
+  img{
+    border: 0;
+    display: block;
+    width: 100%;
+    height:1.42rem;
 
-.mint-swipe
-  height 4.2667rem
-  .one
-    background #ccc
-  .two
-    background #ffc107
-  .three
-    background #656b79
-  .mint-swipe-items-wrap
-    color #fff
-    text-align center
-    .mint-swipe-item
-      line-height 4.2667rem
-
+  }
+}
+// .mint-swipe{
+//   height:4.2667rem;
+//   .one{
+//     background:#ccc;
+//     }
+//   .two{
+//     background:#ffc107;
+//   }
+//   .three{
+//     background:#656b79;
+//     }
+//   .mint-swipe-items-wrap{
+//     color:#fff;
+//     }
+//     text-align:center;
+//     .mint-swipe-item{
+//       line-height:4.2667rem;
+//       }
+// }
 </style>
 
